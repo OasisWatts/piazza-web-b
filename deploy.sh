a95e04d0ec870dd8b142765f825b44ec7882a438
root <root@172-233-129-121.ip.linodeusercontent.com>
2024-03-08 16:55:45 +0000

    nginx changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker-compose up --no-deps --force-recreate -d nodejs1
 printf "Wait for 100 seconds please. Don't turn off.\n"
-sleep 100
+#sleep 100
 docker-compose up --no-deps --force-recreate -d nodejs2
